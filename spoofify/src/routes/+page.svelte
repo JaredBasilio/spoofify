b8ed9869faabdbf402a7b3fe4916c4dce13ecd3a
Jared Andrew Basilio <jaredbasilio02@gmail.com>
2023-12-29 12:56:05 +0800

    Routing

--- spoofify/src/routes/+page.svelte
+++ spoofify/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/privacy">Privacy Policy</a>
+<h1>Spoofify</h1>
+<h2>Fake your 2023 Spotify Wrapped</h2>
+<button>Login to Spotify</button>
+<footer>Made By Jared Andrew Basilio</footer>
---
+++ spoofify/src/routes/privacy/+page.svelte
@@ -0,0 +1,12 @@
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/privacy">Privacy Policy</a>
+<h1>Privacy Policy</h1>
+<p>Spoofify was developed as an open source app powered by the Spotify Web API. By choosing to use this app, you agree to the use of your Spotify account username and data for your top artists and tracks.
+
+
+    None of the data used by Spoofify is stored or collected anywhere, and it is NOT shared with any third parties. All information is used solely for spoofify your Spotify Wrapped.
+    
+    
+    Although you can rest assured that your data is not being stored or used maliciously, if you would like to revoke Spoofify's permissions, you can visit your apps page and click "REMOVE ACCESS" on Spoofify. Here is a more detailed guide for doing so.</p>
+<footer>Made By Jared Andrew Basilio</footer>
