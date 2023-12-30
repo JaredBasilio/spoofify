ad555ed0ca100d4b7830f8910ec51191fa1ea485
Jared Andrew Basilio <jaredbasilio02@gmail.com>
2023-12-30 12:48:46 +0800

    Changed to React

--- spoofify/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<a href="/">Home</a>
-<a href="/about">About</a>
-<a href="/privacy">Privacy Policy</a>
-<h1>Spoofify</h1>
-<h2>Fake your 2023 Spotify Wrapped</h2>
-<button>Login to Spotify</button>
-<footer>Made By Jared Andrew Basilio</footer>
--- spoofify/src/routes/about/+page.svelte
+++
@@ -1,9 +0,0 @@
-<a href="/">Home</a>
-<a href="/about">About</a>
-<a href="/privacy">Privacy Policy</a>
-<h1>About this site</h1>
-<p>Spoofify is a tool  designed to create, save, and export fake Spotify Wrapped 2023 results using Spotify's Web API.</p>
-<h1>FAQ</h1>
-<h2>Will spotify steal my data?</h2>
-<p>In short: NO! For more information, view Spoopify's <a href="/privacy">Privacy Policy</a></p>
-<footer>Made By Jared Andrew Basilio</footer>
